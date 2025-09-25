6049e121d943b3f342361fc6e1672eaf67e8c053
Jemin Kalakoti <jeminkalakoti@gmail.com>
2025-09-24 20:15:26 -0500

    script to create instance and see public, private ip's and update records

--- roboshop.sh
+++ roboshop.sh
@@ -2,6 +2,8 @@
 
 AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-0a3bb5a6589ebd361"
+ZONE_ID="Z0292208N1Q9H7WGUJ5M" # replace with your ID
+DOMAIN_NAME="kalakoti.fun"
 
 for instance in $@  # $1 is the first argument and $@ is all the arguments
 do
@@ -10,8 +12,30 @@ do
     #To get Private IP
     if [ $instance != "frontend" ]; then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+        RECORD_NAME="$instance.$DOMAIN_NAME" #mongodb.kalakoti.fun
     else
-        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)       
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+        RECORD_NAME="$DOMAIN_NAME" #kalakoti.fun     
     fi  
     echo "$instance: $IP"
+
+    #To create A record
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+    {
+        "Comment": "Updating record set"
+        ,"Changes": [{
+        "Action"              : "UPSERT"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$RECORD_NAME'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "'$IP'"
+            }]
+        }
+        }]
+    }
+    '
 done
