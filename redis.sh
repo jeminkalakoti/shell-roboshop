e2c301bb79f792c47c595599ce40f16b57dd4a32
Jemin Kalakoti <jeminkalakoti@gmail.com>
2025-09-27 17:36:21 -0500

    Setting up redis

--- redis.sh
+++ redis.sh
@@ -9,6 +9,7 @@ N='\e[0m'    # No Color
 LOGS_FOLDER="/var/log/shell-roboshop/"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 ) # Extract script name without extension
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # Define log file path
+START_TIME=$(date +%s) # Get the script start time
 
 # -e enables the interpretation of backslash escapes
 
@@ -30,3 +31,27 @@ VALIDATE(){ # Functions receive arguments like normal scripts
 
 }
 
+## Redis Installation Steps ##
+
+dnf module disable redis -y &>>$LOG_FILE # Disable the default redis module
+VALIDATE $? "Disabling Redis module" # Validate the last command
+
+dnf module enable redis:7 -y &>>$LOG_FILE # Enable the redis 7 module
+VALIDATE $? "Enabling Redis 7 module" # Validate the last command
+
+dnf install redis -y &>>$LOG_FILE # Install Redis
+VALIDATE $? "Installing Redis 7"    # Validate the last command
+
+# Update the redis configuration to allow remote connections
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
+VALIDATE $? "Updating Redis Configuration by allowing remote connections"    # Validate the last command
+
+systemctl enable redis &>>$LOG_FILE # Enable Redis service
+VALIDATE $? "Enabling Redis"    # Validate the last command
+
+systemctl start redis &>>$LOG_FILE # Start Redis service
+VALIDATE $? "Starting Redis"    # Validate the last command 
+
+END_TIME=$(date +%s) # Get the script end time
+TOTAL_TIME=$((END_TIME - START_TIME)) # Calculate the total time taken
+echo -e "Total time taken to execute the script:$Y $TOTAL_TIME seconds $N" 
