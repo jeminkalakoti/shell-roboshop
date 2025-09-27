06e0d418c4cd729b18ea21fa2a110136d08aff23
Jemin Kalakoti <jeminkalakoti@gmail.com>
2025-09-27 17:12:01 -0500

    catalogue service using set method

--- catalogue-set.sh
+++ catalogue-set.sh
@@ -1,61 +1,72 @@
 #!/bin/bash
 
-set -euo pipefail
+set -euo pipefail # Exit on error, undefined variable, and error in a pipeline
+# set -e
+# set -u
+# set -o pipefail   
 
-trap 'echo "There is an error in $LINENO, Command is: $BASH_COMMAND"' ERR
+trap 'echo "There is an error in $LINENO, Command is: $BASH_COMMAND"' ERR # Trap ERR signal and print the line number and command that caused the error
 
-USERID=$(id -u)
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-LOGS_FOLDER="/var/log/shell-roboshop"
-SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
-SCRIPT_DIR=$PWD
-MONGODB_HOST=mongodb.daws86s.fun
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
 
-mkdir -p $LOGS_FOLDER
-echo "Script started executed at: $(date)" | tee -a $LOG_FILE
+USER_ID=$(id -u)
+R='\e[0;31m' # Red
+G='\e[0;32m' # Green
+Y='\e[0;33m' # Yellow
+N='\e[0m'    # No Color
 
-if [ $USERID -ne 0 ]; then
-    echo "ERROR:: Please run this script with root privelege"
-    exit 1 # failure is other than 0
+LOGS_FOLDER="/var/log/shell-roboshop/"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 ) # Extract script name without extension
+SCRIPT_DIR=$PWD # Get the current working directory
+MONGODB_HOST="mongodb.kalakoti.fun" # MongoDB Host
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # Define log file path
+
+# -e enables the interpretation of backslash escapes
+
+mkdir -p $LOGS_FOLDER # Create logs folder if not exists
+echo "Script started and executed at : $(date)" | tee -a $LOG_FILE # Log script start time
+
+if [ $USER_ID -ne 0 ]; then
+    echo -e "$R ERROR $N:: You must have a root privilege to install packages" 
+    exit 1 # Exit the script if not root
 fi
 
-##### NodeJS ####
-dnf module disable nodejs -y &>>$LOG_FILE
-dnf module enable nodejs:20 -y  &>>$LOG_FILE
-dnf install nodejs -y &>>$LOG_FILE
-echo -e "Installing NodeJS 20 ... $G SUCCESS $N"
 
-id roboshop &>>$LOG_FILE
+###  Catalogue Application Installation Steps NodeJS Application ###
+
+dnf module disable nodejs -y &>>$LOG_FILE # Disable the default nodejs module
+dnf module enable nodejs:20 -y &>>$LOG_FILE # Enable the nodejs 20 module
+dnf install nodejs -y &>>$LOG_FILE # Install NodeJS
+echo -e "Installing NodeJS:20 ... $G SUCCESSFUL $N"   # Print success message in green color
+
+id roboshop &>>$LOG_FILE # Check if the user already exists
 if [ $? -ne 0 ]; then
-    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE # Add application user
 else
-    echo -e "User already exist ... $Y SKIPPING $N"
+    echo -e "User roboshop already exists. $Y SKIPPING $N" # Print skipping message in yellow color
 fi
 
-mkdir -p /app
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>$LOG_FILE
+mkdir -p /app #Create application directory if not exists 
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>$LOG_FILE # Download the application code
 cd /app 
-rm -rf /app/*
-unzip /tmp/catalogue.zip &>>$LOG_FILE
-npm install &>>$LOG_FILE
-cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service
-systemctl daemon-reload
-systemctl enable catalogue &>>$LOG_FILE
-echo -e "Catalogue application setup ... $G SUCCESS $N"
+rm -rf /app/* # Remove any existing application code
+unzip /tmp/catalogue.zip &>>$LOG_FILE # Unzip the application code
+npm install &>>$LOG_FILE # Download the application dependencies
+cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service # Copy the service file
+systemctl daemon-reload # Reload systemd to register the service
+systemctl enable catalogue &>>$LOG_FILE # Enable the service
+systemctl start catalogue # Start the service
+echo -e "catalogue application setup completed ... $G SUCCESSFUL $N"   # Print success message in green color
 
-cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
-dnf install mongodb-mongoshsfds -y &>>$LOG_FILE
+cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo # Copy the repo file
+dnf install mongodb-mongosh -y &>>$LOG_FILE # Install MongoDB Shell
 
-INDEX=$(mongosh mongodb.daws86s.fun --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
+INDEX=$(mongosh mongodb.kalakoti.fun --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
 if [ $INDEX -le 0 ]; then
-    mongosh --host $MONGODB_HOST </app/db/master-data.js &>>$LOG_FILE
+    mongosh --host $MONGODB_HOST </app/db/master-data.js &>>$LOG_FILE # Load the schema
+    VALIDATE $? "Loading Catalogue Products to MongoDB"    # Validate the last command
 else
-    echo -e "Catalogue products already loaded ... $Y SKIPPING $N"
-fi
+    echo -e "Catalogue Products already exists. $Y SKIPPING $N" # Print skipping message in yellow color
+fi  
 
-systemctl restart catalogue
-echo -e "Loading products and restarting catalogue ... $G SUCCESS $N"
+systemctl restart catalogue # Restart the service
+echo -e "Loading Prodcuts and Restarting catalogue service ... $G SUCCESSFUL $N"   # Print success message in green color.
